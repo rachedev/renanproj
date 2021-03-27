033afee4513f10d545e76be22cac429a8d428b67
Rachelle Lee <rachedeveloper@gmail.com>
2021-03-27 16:18:56 -0700

    after wk4 assignment

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-import React from "react";
-import Main from "./components/MainComponent";
-import { Provider } from "react-redux";
-import { ConfigureStore } from "./redux/configureStore";
-import { PersistGate } from "redux-persist/es/integration/react";
-import Loading from "./components/LoadingComponent";
+import React from 'react';
+import Main from './components/MainComponent';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
+import { PersistGate } from 'redux-persist/es/integration/react';
+import Loading from './components/LoadingComponent';
+console.disableYellowBox = true;
 
 const { persistor, store } = ConfigureStore();
 
